09f91e3bf6ba526c5b56dd07c7237947d8f3b8af
Dhemeira <peterlakics2@gmail.com>
2023-03-21 08:54:10 +0100

    style(router/index.js): remove unnecessary comment

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router';
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory('/'),
   scrollBehavior(to, from, savedPosition) {
     if (to.hash) {
       return {
@@ -14,9 +14,6 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
       component: () => import('../views/HomeView.vue'),
       meta: { title: 'Home' },
     },
